520f66e394f78a317f84e1150d3337a30e934ce1
luisa <meron.camar24@triana.salesianos.edu>
2024-10-15 12:30:31 +0200

    calculadora

---
+++ src/app/components/calculadora/calculadora.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+
+  calculo = 0;
+
+  addNum(e: number) { // arg0 ? e ?
+    {{this.addNum}} ;
+  }
+  
+  //restar()
+
+  resultado = 0;
+}
